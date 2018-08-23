a62af6f2f7e827c37363a0a394aff70369c6868b
CrazyMerlyn <crazy.d.merlyn@gmail.com>
2018-08-23 15:06:19 +0530

    Implement a simple dns query using libuv

---
+++ dns.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+#include <uv.h>
+
+uv_loop_t *loop;
+
+void on_resolved(uv_getaddrinfo_t *resolver, int status, struct addrinfo *res) {
+    if (status < 0) {
+        fprintf(stderr, "getaddrinfo callback error %s\n", uv_err_name(status));
+        return;
+    }
+
+    char addr[17] = {0};
+    uv_ip4_name((struct sockaddr_in *) res->ai_addr, addr, 16);
+    fprintf(stderr, "%s\n", addr);
+
+    uv_connect_t *connect_req = (uv_connect_t *)malloc(sizeof(uv_connect_t));
+    uv_tcp_t *socket = (uv_tcp_t*) malloc(sizeof(uv_tcp_t));
+    uv_tcp_init(loop, socket);
+
+    uv_tcp_connect(connect_req, socket, (const struct sockaddr *) res->ai_addr, NULL);
+
+    uv_freeaddrinfo(res);
+}
+
+int main(int argc, const char *argv[])
+{
+    loop = uv_default_loop();
+
+    struct addrinfo hints;
+    hints.ai_family = PF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+    hints.ai_flags = 0;
+
+    uv_getaddrinfo_t resolver;
+    fprintf(stderr, "irc.freenode.net is...");
+    int r = uv_getaddrinfo(loop, &resolver, on_resolved, "irc.freenode.net", "6667", &hints);
+
+    if (r) {
+        fprintf(stderr, "getaddrinfo call error %s\n", uv_err_name(r));
+        return 1;
+    }
+
+    return uv_run(loop, UV_RUN_DEFAULT);
+}
+
