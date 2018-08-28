d592dea6a4d763d60bca024f550c4b7f3f6b1335
CrazyMerlyn <crazy.d.merlyn@gmail.com>
2018-08-28 15:19:57 +0530

    Add cancel ability to work queue

--- queue-work.c
+++ queue-work.c
@@ -3,7 +3,19 @@
 #include <unistd.h>
 #include <uv.h>
 
+#define FIB_UNTIL 10
+
 uv_loop_t *loop;
+uv_work_t fib_reqs[FIB_UNTIL];
+int data[FIB_UNTIL];
+
+void signal_handler(uv_signal_t *req, int signum) {
+    printf("Signal received!\n");
+    for (int i = 0; i < FIB_UNTIL; ++i) {
+        uv_cancel((uv_req_t *)&fib_reqs[i]);
+    }
+    uv_signal_stop(req);
+}
 
 long fib_(int n) {
     long a = 0, b = 1;
@@ -24,21 +36,25 @@ void fib(uv_work_t *req) {
 }
 
 void after_fib(uv_work_t *req, int status) {
+    if (status == UV_ECANCELED) {
+        fprintf(stderr, "Calculation of %dth fibonacci cancelled\n", *(int *)req->data);
+        return;
+    }
     fprintf(stderr, "Done calculating %dth fibonacci\n", *(int *)req->data);
 }
-
-#define FIB_UNTIL 10
 
 int main() {
     loop = uv_default_loop();
 
-    int data[FIB_UNTIL];
-    uv_work_t req[FIB_UNTIL];
     for (int i = 0; i < FIB_UNTIL; ++i) {
         data[i] = i;
-        req[i].data = &data[i];
-        uv_queue_work(loop, &req[i], fib, after_fib);
+        fib_reqs[i].data = &data[i];
+        uv_queue_work(loop, &fib_reqs[i], fib, after_fib);
     }
+
+    uv_signal_t sig;
+    uv_signal_init(loop, &sig);
+    uv_signal_start(&sig, signal_handler, SIGINT);
 
     return uv_run(loop, UV_RUN_DEFAULT);
 }
